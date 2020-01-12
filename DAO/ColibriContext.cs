33d3ed6e56fcec0780c88325aeb6ead46952a3a7
BlackLight1613 <eldin.anthony@gmail.com>
2020-01-12 21:28:32 +0100

    Mise en place d'EF6
    Validation de la connexion et du contexte

---
+++ DAO/ColibriContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace Colibri.DAO
+{
+    public class ColibriContext:DbContext
+    {
+        public ColibriContext():base("ColibriContext")
+        {
+
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+        public System.Data.Entity.DbSet<Colibri.Models.Supplier> Suppliers { get; set; }
+    }
+}
