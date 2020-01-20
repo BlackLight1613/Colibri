53719a26648aa9290ea3ce65662badfe4eb3f54c
BlackLight1613 <eldin.anthony@gmail.com>
2020-01-20 22:55:43 +0100

    Création commande
    Début du script d'ajout d'articles

--- Content/JS/commandArticlesCreate.js
+++ Content/JS/commandArticlesCreate.js
@@ -1,3 +1,16 @@
 ﻿{
-    console.log('commandArticleCreate');
+    const init = () => {
+        dom = {
+            addArticleForm: document.getElementById('article_form'),
+            showArticleFormButton: document.getElementById('CreateArticleFormButton'),
+            hideArticleFormButton: document.getElementById('CancelArticleFormButton')
+        };
+        //Enleve le formulaire d'ajout d'un article au chargement de la page
+        dom.addArticleForm.style.display = 'none';
+
+        //Permet d'afficher le formlaire d'ajout d'un article
+        dom.showArticleFormButton.addEventListener('click', () => { dom.addArticleForm.style.display = ''; });
+    };
+
+    document.addEventListener('DOMContentLoaded', ()=>init());
 }
