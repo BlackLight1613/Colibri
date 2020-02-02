9c47d9e681d13dc25d819932bc06142f8face6e6
BlackLight1613 <eldin.anthony@gmail.com>
2020-02-02 21:20:16 +0100

    Création commande
    Mise en place du controlleur ok .
    Formaulaire articles ok

--- Controllers/CommandArticlesController.cs
+++ Controllers/CommandArticlesController.cs
@@ -11,15 +11,28 @@ namespace Colibri.Controllers
     public class CommandArticlesController :Controller
     {
         private ColibriContext db = new ColibriContext();
+        //private Command Command { get; set; }
 
         public ActionResult Create(Command command)
         {
             List<Unity> Unities = db.Unities.ToList();
+            //Command = command;
             ViewData["Unities"] = Unities;
             ViewData["Command"] = command;
             if (command == null)
                 RedirectToAction("Index");
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ArticleList(int CommandId)
+        {
+            //A voir https://stackoverflow.com/questions/27896202/pass-table-value-from-view-to-controller-mvc/27896825#27896825
+            //Récupératon de la commande 
+            Command command = db.Commands.Find(CommandId);
+            System.Diagnostics.Debug.WriteLine("OnPostArticleList déclenché. Commande en cours => "+ command.Supplier.Name);
+            return null;
         }
     }
 }
